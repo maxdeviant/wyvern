bd51f411e781c80f4827258590807b741c25f131
Marshall Bowers <elliott.codes@gmail.com>
2018-01-13 15:44:43 -0500

    Update copyright info

--- src/Wyvern/AssemblyInfo.fs
+++ src/Wyvern/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Wyvern")>]
-[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyCopyright("Copyright © Marshall Bowers 2018")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
