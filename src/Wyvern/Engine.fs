c3795c119a24e41b97f4aa61347bcd378208a024
Marshall Bowers <elliott.codes@gmail.com>
2018-01-13 17:48:34 -0500

    Begin adding Wyvern engine

---
+++ src/Wyvern/Engine.fs
@@ -0,0 +1,41 @@
+﻿module Wyvern.Engine
+
+open Microsoft.Xna.Framework
+
+type private GameEngine(loadContent: unit -> unit, unloadContent: unit -> unit) as this =
+  inherit Game()
+
+  do this.IsFixedTimeStep <- false
+  do this.Content.RootDirectory <- "Content"
+  let graphics = new GraphicsDeviceManager(this)
+  // @TODO Set vsync based on user settings.
+  do graphics.SynchronizeWithVerticalRetrace <- false
+
+  override __.Initialize() =
+    base.Initialize()
+
+  override __.LoadContent() =
+    loadContent()
+
+  override __.UnloadContent() =
+    unloadContent()
+
+  override __.Update gameTime =
+    base.Update gameTime
+
+  override __.Draw gameTime =
+    base.Draw gameTime
+
+let init loadContent unloadContent =
+  // @TODO: The engine never gets disposed, so this may create the possibility
+  // for a memory leak. Will need to investigate further to determine whether
+  // it is actually an issue.
+  let engine = new GameEngine(loadContent, unloadContent)
+  
+  let run () =
+    engine.Run()
+
+  let exit () =
+    engine.Exit()
+
+  run, exit
---
+++ src/Wyvern/MonoGameInterop.fs
@@ -0,0 +1,6 @@
+﻿module internal Wyvern.MonoGameInterop
+
+open Microsoft.Xna.Framework
+
+let toVector2 (vector2: Wyvern.Vector2) =
+  new Vector2(float32 vector2.x, float32 vector2.y)
