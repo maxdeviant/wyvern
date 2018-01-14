6843cc31eabcdb2c38bf6a34c51d32d8c45ad34d
Marshall Bowers <elliott.codes@gmail.com>
2018-01-13 22:02:51 -0500

    Make Texture disposable

--- src/Wyvern/Graphics.fs
+++ src/Wyvern/Graphics.fs
@@ -1,17 +1,22 @@
 ﻿module Wyvern.Graphics
 
 open Microsoft.Xna.Framework.Graphics
+open System
 open System.IO
 
 type Texture internal (texture: Texture2D) =
   member internal __.Texture = texture
   member __.Dimensions = (texture.Width, texture.Height)
 
+  interface IDisposable with
+    override __.Dispose() =
+      __.Texture.Dispose()
+
 let loadTexture graphicsDevice contentRoot path =
   let absolutePath = Path.Combine (contentRoot path)
   try
     use fileStream = new FileStream(absolutePath, FileMode.Open)
-    Some (Texture (Texture2D.FromStream(graphicsDevice, fileStream)))
+    Some (new Texture(Texture2D.FromStream(graphicsDevice, fileStream)))
   with
   | _ -> None
 
@@ -19,4 +24,4 @@ let createTexture graphicsDevice (width, height) color =
   let texture = new Texture2D(graphicsDevice, width, height)
   let colorData = [| for _ in 0 .. (width * height) - 1 -> Wyvern.MonoGameInterop.toColor color |]
   texture.SetData colorData
-  Texture texture
+  new Texture(texture)
