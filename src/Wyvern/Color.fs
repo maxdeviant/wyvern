938b4857723f153d50d26a66a67c2600c8a709c5
Marshall Bowers <elliott.codes@gmail.com>
2018-01-13 22:12:28 -0500

    Remove unneeded code

--- src/Wyvern/Color.fs
+++ src/Wyvern/Color.fs
@@ -17,14 +17,3 @@ module Color =
 
   let a color =
     match color with Color value -> byte value
-  //open System
-  //open Math
-
-  //let rgb r g b alpha =
-  //  if ((r ||| g ||| b ||| alpha) &&& 0xffffff00) <> 0
-  //  then
-  //    let clamp' = Math.clamp (int Byte.MinValue) (int Byte.MaxValue)
-  //    let packedValue = ((clamp' alpha) <<< 24) ||| ((clamp' b) <<< 16) ||| ((clamp' g) <<< 8) ||| (clamp' r)
-  //    Color (uint32 packedValue)
-  //  else Color 
-  //    //Color ((clampedR <<< 24) ||| (clampedG <<< 16) ||| (clampedB <<< 8))
